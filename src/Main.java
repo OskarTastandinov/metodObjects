218720e10b2ee77bd3bbfe5e8cbe6b0048277a04
askar <tashtandinov.oskar@yandex.ru>
2022-10-11 20:53:32 +0700

    commit1

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        Author SergeyEsenin = new Author("Сергей", "Есенин");
-        Author AleksandrPushkin = new Author("Александр","Пушкин");
-
-
-        Book LukDub = new Book("У лукоморья дуб зелёный",AleksandrPushkin,1820);
-        Book PismoKZhenshine = new Book("Письмо к женщине",SergeyEsenin,1924);
-
-        System.out.println(LukDub);
-        System.out.println(PismoKZhenshine);
-        System.out.println();
-
-
-
-
+        System.out.println("fdf");
     }
 }
