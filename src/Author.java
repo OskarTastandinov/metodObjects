3cb47e11c633c32adcbd75d4042d932c192d2ac8
askar <tashtandinov.oskar@yandex.ru>
2022-10-11 21:03:25 +0700

    commit1

--- src/Author.java
+++ src/Author.java
@@ -1,7 +1,40 @@
 import java.util.Objects;
 
 public class Author {
-   public static void dfd(String[] args){
-       System.out.println("fd");
-   }
+    private final String surname;
+    private final String name;
+
+    public Author(String name,String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getSurname(){
+        return this.surname;
+    }
+    public String toString(){
+        return ", Автор книги: " + getName() + " " + getSurname();
+    }
+    @Override
+    public boolean equals(Object obj){
+        if (obj == null){
+            return false;
+        }
+        if(this == obj){
+            return true;
+        }
+        if(!(obj instanceof Author)){
+            return false;
+        }
+        Author that = (Author) obj;
+        return this.name.equals(that.name) && this.surname.equals(that.surname);
+
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(name,surname);
+    }
+
 }
